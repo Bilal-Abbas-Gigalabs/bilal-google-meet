04873d7555ccfb752756cd137e2189efb5f927a6
Bilal Abbas <bilal.abbas@gigalabs.co>
2022-09-27 19:27:25 +0500

    left video after 20s

--- app.js
+++ app.js
@@ -28,15 +28,18 @@ const username = "Flair Assistant";
   await page.type('input[type="text"]', username);
   await page.keyboard.press('Enter');
 
-  // await page.waitForSelector('[class*="NHaLPe"] span button]', { visible: true })
-  // await page.click('[class*="NHaLPe"] span button]');
+  await page.waitForTimeout(20000);
 
-  // const info = await page.$eval('#ow3 .roSPhc');
-  // console.log(info)
-  // if(info == 'You left the meeting')
-  //   await recorder.stop();
-  // else
-  await page.waitForTimeout(200000);
-  await recorder.stop();
-  await browser.close();
+  await page.waitForSelector('#ow3 > div.T4LgNb > div > div:nth-child(11) > div.crqnQb > div.UnvNgf.Sdwpn.P9KVBf > div.Tmb7Fd > div > div.NHaLPe > span > button', { visible: true })
+  await page.click('#ow3 > div.T4LgNb > div > div:nth-child(11) > div.crqnQb > div.UnvNgf.Sdwpn.P9KVBf > div.Tmb7Fd > div > div.NHaLPe > span > button');
+
+  await page.waitForTimeout(10000);
+  const info = await page.$eval('#ow3 > div > div > div.roSPhc', element => element.textContent);
+  if(info == 'You left the meeting'){
+    await recorder.stop();
+    await browser.close();
+  }
+  else{
+    await page.waitForTimeout(200000);
+  }
 })();
