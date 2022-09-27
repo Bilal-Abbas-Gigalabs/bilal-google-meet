7c08d4f8d7a59e8e457e5b2c4263e053a0dd11f8
Bilal Abbas <bilal.abbas@gigalabs.co>
2022-09-27 19:45:16 +0500

    close browser and stop recording on end of meeting

--- app.js
+++ app.js
@@ -28,18 +28,13 @@ const username = "Flair Assistant";
   await page.type('input[type="text"]', username);
   await page.keyboard.press('Enter');
 
-  await page.waitForTimeout(20000);
-
-  await page.waitForSelector('#ow3 > div.T4LgNb > div > div:nth-child(11) > div.crqnQb > div.UnvNgf.Sdwpn.P9KVBf > div.Tmb7Fd > div > div.NHaLPe > span > button', { visible: true })
-  await page.click('#ow3 > div.T4LgNb > div > div:nth-child(11) > div.crqnQb > div.UnvNgf.Sdwpn.P9KVBf > div.Tmb7Fd > div > div.NHaLPe > span > button');
-
-  await page.waitForTimeout(10000);
-  const info = await page.$eval('#ow3 > div > div > div.roSPhc', element => element.textContent);
-  if(info == 'You left the meeting'){
-    await recorder.stop();
-    await browser.close();
-  }
-  else{
-    await page.waitForTimeout(200000);
-  }
+  setInterval(async function () {
+    await page.waitForSelector('#ow3 > div > div > div.roSPhc', { visible: true }).then(async () => {
+        recorder.stop();
+        await browser.close();
+        process.exit(1);
+    }).catch(e => {
+      console.log('FAIL');
+    });
+  }, 1000);
 })();
